dee6593220952f1913d1feff882baf2311c9fb73
Tahsin <tasin9209@gmail.com>
2025-08-14 23:57:18 +0600

    added shorebird again

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,19 +1,16 @@
 plugins {
-    [cite_start]id("com.android.application") [cite: 1]
-    [cite_start]id("kotlin-android") [cite: 1]
+    id("com.android.application")
+    id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    [cite_start]id("dev.flutter.flutter-gradle-plugin") [cite: 2]
+    id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    [cite_start]namespace = "com.example.rahbar_restarted" [cite: 3]
+    namespace = "com.example.rahbar_restarted"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "29.0.13846066"
+    ndkVersion = "29.0.13846066" // This might be different for you
 
     compileOptions {
-        // ===== পরিবর্তন ১: এই লাইনটি যোগ করা হয়েছে =====
-        coreLibraryDesugaringEnabled = true
-
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -23,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        [cite_start]applicationId = "com.example.rahbar_restarted" [cite: 3]
-        // You can update the following values to match your application needs.
-        [cite_start]// For more information, see: https://flutter.dev/to/review-gradle-config. [cite: 4]
+        applicationId = "com.example.rahbar_restarted"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -35,18 +29,11 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            [cite_start]// Signing with the debug keys for now, so `flutter run --release` works. [cite: 5]
-            [cite_start]signingConfig = signingConfigs.getByName("debug") [cite: 6]
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
 flutter {
     source = "../.."
-}
-
-// ===== পরিবর্তন ২: এই সম্পূর্ণ ব্লকটি ফাইলের শেষে যোগ করা হয়েছে =====
-dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
